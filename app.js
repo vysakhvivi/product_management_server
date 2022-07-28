1689ffc48b1d03a9c16b66a872f39e5cbad4b5d1
vysakh <vivimnair@gmail.com>
2022-07-29 05:14:38 +0530

    added middleware and mongoose installed

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const expresss= require('express')
 
+const mongoose=require('mongoose')
+
+
+
 const app = expresss();
+
+const db='mongodb+srv://vysakhmnair:vysakhvivi007@cluster0.vns9c1c.mongodb.net/product?retryWrites=true&w=majority'
+
+
+
+
+mongoose.connect(db,{
+    
+    maxPoolSize: 50,
+    wtimeoutMS: 2500,
+    useNewUrlParser: true,
+   
+}).then(()=>{
+    console.log('connection successfull');
+}).catch((err)=>console.log('cant connect'))
+
+
+const middleware=(req,res,next)=>{
+console.log('middleware')
+}
+
+middleware();
 
 app.get('/',(req,res) =>{
 res.send('testing from backend')

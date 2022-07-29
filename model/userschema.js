ae6673a9b28f8405e37e9e02d7a9be8cca5199b5
vysakh <vivimnair@gmail.com>
2022-07-29 09:39:41 +0530

    backend Almost Over

--- model/userschema.js
+++ model/userschema.js
@@ -1,25 +1,59 @@
-const mongoose=require('mongoose')
+const jwt=require('jsonwebtoken')
+const mongoose = require('mongoose')
 
-const userschema=new mongoose.Schema({
-    name:{
+const bcrypt = require('bcryptjs')
+
+const userschema = new mongoose.Schema({
+    name: {
         type: String,
-        required:true
+        required: true
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+        type: String,
+        required: true
 
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    place:{
-        type:String,
-        required:true
-    }
+    place: {
+        type: String,
+        required: true
+    },
+    tokens:
+        [{
+            token: {
+                type: String,
+                required: true
+            }
+        }
+        ]
+
+
 })
 
-const User=mongoose.model('USER',userschema)
+
+
+userschema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next();
+});
+
+userschema.methods.generateAuthToken = async function () {
+    try {
+        let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({ token: token })
+        await this.save();
+        return token;
+    }
+    catch (err) { 
+        console.log("err")
+    }
+}
+
+const User = mongoose.model('USER', userschema)
 
 module.exports = User;
--- router/auth.js
+++ router/auth.js
@@ -1,54 +1,74 @@
-const express= require('express')
+const express = require('express')
 
 const router = express.Router();
 
+const bcrypt = require('bcryptjs')
+
+const jwt= require('jsonwebtoken')
+
 require("../db/conn");
 
-const User=require("../model/userschema");
+const User = require("../model/userschema");
 
-router.get('/',(req,res) =>{
+router.get('/', (req, res) => {
     res.send('testing from backend router');
-    });
+});
 
-router.post('/', async (req,res)=>{
-const {name, email, password, place}=req.body;
+router.post('/', async (req, res) => {
+    const { name, email, password, place } = req.body;
 
-    if(!name || !email || !password|| !place)
-    {
-        return res.status(422).json({error:"Enter the required fields"})
+    if (!name || !email || !password || !place) {
+        return res.status(422).json({ error: "Enter the required fields" })
     }
 
-    User.findOne({email:email})
-    .then((userAlreadyExist)=>{
-        if(userAlreadyExist){
-            return res.status(422).json({error:"Email already Exist"});
-        }
-        const user=new User({name, email, password, place});
+    User.findOne({ email: email })
+        .then((userAlreadyExist) => {
+            if (userAlreadyExist) {
+                return res.status(422).json({ error: "Email already Exist" });
+            }
+            const user = new User({ name, email, password, place });
 
-        user.save().then(()=>{
-            res.status(201).json({message: "user details successfully saved"})
-        }).catch((err)=>res.status(500).json({error:"failed to store the data"}))
-    }).catch((err)=>{console.log(err);})
+            user.save().then(() => {
+                res.status(201).json({ message: "user details successfully saved" })
+            }).catch((err) => res.status(500).json({ error: "failed to store the data" }))
+        }).catch((err) => { console.log(err); })
 })
 
-router.post('/login',async (req,res)=>{
-        try{
-            const {email, password} = req.body;
-            if(!email || !password){
-                return res.status(400).json({error:"Please Enter the data correctly"})
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ error: "Please Enter the data correctly" })
+        }
+        const userlogin = await User.findOne({ email: email });
+
+        if (userlogin) {
+            const samepass = await bcrypt.compare(password, userlogin.password);
+        
+            const token = await userlogin.generateAuthToken();
+
+            res.cookie("jwtoken",token,{
+                expires:new Date(Date.now()+3000000000),
+                httpOnly:true
+            })
+
+            if (samepass) {
+                res.status(200).json({ message: "Successfully signed-in" })
             }
-            const userlogin= await User.findOne({email:email});
-            if(!userlogin){
-                res.status(400).json({message:"Error. Please check the entered details"})
-            }
-            else
-            {
-                res.json({message:"Sign-in successfull"})
+            else{
+                res.status(400).json({error:"Invalid data"})
             }
         }
-        catch(err){
-            console.log(err);
+        else {
+            res.status(400).json({ error: "Please check the entered data" })
         }
+
+
+
+    }
+    catch (err) {
+        console.log(err);
+    }
 })
 
 
